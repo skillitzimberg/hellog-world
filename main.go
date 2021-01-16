900c099a62ce943b1976d155180438a559ab484b
Scott Bergler <commandinghands@gmail.com>
2021-01-16 12:20:20 -0800

    Create separate log and output files

--- main.go
+++ main.go
@@ -9,8 +9,15 @@ func main() {
 	lf, err := os.OpenFile("logs.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
 	defer lf.Close()
 	if err != nil {
-		log.Printf("OpenFile failed:%s", err)
+		log.Printf("Opening log file failed:%s", err)
 	}
 	log.SetOutput(lf)
-	log.Println("Hello, World!")
+	log.Println("Hellog, World!")
+
+	cf, err := os.Create("output.txt")
+	if err != nil {
+		log.Printf("Creating output file failed: %s", err)
+	}
+
+	cf.Write([]byte("Here are your results: . . ."))
 }
